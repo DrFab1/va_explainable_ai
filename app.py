cc2fefa35ef54390e338240e751d1513154c72ef
Max <max.rudeck@gmx.de>
2021-06-20 17:42:59 +0200

    dash dashboard

--- app.py
+++ app.py
@@ -1,40 +1,41 @@
-# -*- coding: utf-8 -*-
 
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import plotly.express as px
-import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+df = px.data.iris()
+all_dims = ['sepal_length', 'sepal_width', 
+            'petal_length', 'petal_width']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+app.layout = html.Div([
+    html.Label(["Select Features to use for regression",dcc.Dropdown(
+        id="dropdown",
+        options=[{"label": x, "value": x} 
+                 for x in all_dims],
+        value=all_dims[:2],
+        multi=True
+    )]),
+    html.Label(["Select Target for regression",dcc.Dropdown(
+        id="dropdown2",
+        options=[{"label": x, "value": x} 
+                 for x in all_dims],
+        value=all_dims[:2],
+        multi=True
+    )]),
+    dcc.Graph(id="splom"),
 ])
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+@app.callback(
+    Output("splom", "figure"), 
+    [Input("dropdown", "value")])
+def update_bar_chart(dims):
+    fig = px.scatter_matrix(
+        df, dimensions=dims, color="species")
+    return fig
+
+app.run_server(debug=True)
